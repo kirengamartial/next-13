e3c34e7d883fe5451b34c04da931f073e56d787b
kirengamartial <kirengamartial@gmail.com>
2024-07-23 00:08:49 +0200

    nextjs 13

---
+++ app/about/page.js
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "About Martial Media"
+}
+
+const AboutPage = () => {
+  return (
+    <div>
+        <h1>Welcome Martial</h1>
+        <p>
+          Lorem ipsum afnalfnafnalfn nl lanfnlal nlanfla lan la l nlafhafon afanfagal lafalfon af lanfaofnafaofhn ao aofoan onaofn o aofh oan aofa oafao oafao aoa noa oaofafafo oa aofao aofoafo aofafaogaofoq  afa aaogha oaofaofoa a afafhgaohgoago a oafao
+        </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/about/team/page.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem40 anflanflaffffffffffffffffffffnallllllllllllllll laaaaaaaaaaaaaaaaaaaaaaaaaaaaaa alfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffvvvvvvvvvvvvvvvva  
+      </p>
+    </div>
+  )
+}
+
+export default TeamPage
